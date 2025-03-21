46da4c9c37af404b41e27fbca73986346d02675d
EldritchCraft <83508507+EldritchCraft@users.noreply.github.com>
2025-03-20 20:09:18 -0500

    Update KeyInputHandler.java

--- src/main/java/elocin/shield_overhaul/event/KeyInputHandler.java
+++ src/main/java/elocin/shield_overhaul/event/KeyInputHandler.java
@@ -23,13 +23,12 @@ public class KeyInputHandler {
             if (stack.getNbt() == null) return;
 
             if (client.player.isBlocking()) {
-                if (client.options.attackKey.isPressed() && client.options.attackKey.wasPressed()) {
                     if (!client.player.getItemCooldownManager().isCoolingDown(stack.getItem())) {
                         AnimUtils.playAnimation(client.player, "bash_right", client.player.getActiveHand().equals(Hand.OFF_HAND));
                     }
                     ClientPlayNetworking.send(PacketRegistry.SHIELD_BASH, PacketByteBufs.empty());
                 }
-            }
+            
 
             if (client.player.getItemUseTime() == 1 && !stack.getNbt().getBoolean("holdStarted")) {
                 ClientPlayNetworking.send(PacketRegistry.HOLD_BEGIN, PacketByteBufs.empty());
